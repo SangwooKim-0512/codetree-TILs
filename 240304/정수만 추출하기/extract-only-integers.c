b514637c5d09f1610080588b844f6bdaef770293
SangwooKim-0512 <161012224+SangwooKim-0512@users.noreply.github.com>
2024-03-04 17:11:29 +0900

    Title: 정수만 추출하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240304/정수만 추출하기/extract-only-integers.c
+++ 240304/정수만 추출하기/extract-only-integers.c
@@ -3,21 +3,29 @@
 #include <stdlib.h>
 
 int main() {
-    char str1[8], str2[8], str1_1[8], str2_1[8];
-    scanf("%s%s", str1, str2);
-    int cnt = 0, sum = 0;
+    char str1[9], str2[9], str1_1[9], str2_1[9];
+    scanf("%s %s", str1, str2);
+    int sum = 0;
     for(int i = 0; i < strlen(str1); i++){
         if(str1[i] >= '0' && str1[i] <= '9') str1_1[i] = str1[i];
-        else break;
+        else {
+            str1_1[i] = '\0';
+            break;
+        }
     }
+
     sum += atoi(str1_1);
+
 
     for(int i = 0; i < strlen(str2); i++){
         if(str2[i] >= '0' && str2[i] <= '9') str2_1[i] = str2[i];
-        else break;
+        else {
+            str1_1[i] = '\0';
+            break;
+        }
     }
     sum += atoi(str2_1);
-
     printf("%d", sum);
+
     return 0;
 }
