e59568c50c30b948613ea924c3313a08f13b3c0f
SangwooKim-0512 <161012224+SangwooKim-0512@users.noreply.github.com>
2024-03-04 17:28:33 +0900

    Title: 문자열의 개수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240304/문자열의 개수/number-of-spring.c
+++ 240304/문자열의 개수/number-of-spring.c
@@ -5,13 +5,14 @@ int main() {
     char str1[201], str2[200][201];
     int cnt = 0;
     while(1){
-        if(str1[0] == '0') break;
         scanf("%s", str1);
+        if(str1[0] == '0') break;
         for(int i = 0; i< strlen(str1); i++){
             str2[cnt][i] = str1[i];
         }
         cnt ++;
     }
+    printf("%d\n", cnt);
     for(int i = 0; i < cnt; i++){
         if(i % 2 == 0){
             printf("%s\n", str2[i]);
