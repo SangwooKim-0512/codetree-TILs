499b4d8ad107326eb748955333f1ffea036d04ea
SangwooKim-0512 <161012224+SangwooKim-0512@users.noreply.github.com>
2024-03-04 18:39:08 +0900

    Title: 함수를 이용한 369 게임, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool Contains369(int n){
+    while (n > 0){
+        if(n % 10 == 3 || n % 10 == 6 || n % 10 == 9)
+            return true;
+        n /= 10;
+    }
+    return false;
+}
+bool Is369Number(int n){
+    return Contains369(n) || (n % 3 == 0);
+}
+
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int cnt = 0;
+    for(int i = a; i <= b; i++)
+        if(Is369Number(i)) cnt++;
+
+    cout << cnt;
+
+    return 0;
+}
