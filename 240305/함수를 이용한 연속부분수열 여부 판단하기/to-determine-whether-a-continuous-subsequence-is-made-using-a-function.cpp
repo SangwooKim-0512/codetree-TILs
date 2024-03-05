db8639eeeb4d1bbeba023d99bec3abaea0407ae7
SangwooKim-0512 <161012224+SangwooKim-0512@users.noreply.github.com>
2024-03-05 16:03:22 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 240305/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -6,11 +6,12 @@ int n1, n2;
 bool JudgeArr(int *a, int *b){
     if(n1 < n2) return false;
     for(int i = 0; i <= n1-n2; i++){
-        for(int j = 0; j < n2; j++){
-            if(a[i+j] != b[j]) return false;
-        }
+        int cnt = 0;
+        for(int j = 0; j < n2; j++)
+            if(a[i+j] == b[j]) cnt ++;
+        if(cnt == n2) return true;
     }
-    return true;
+    return false;
 }
 
 int main() {
