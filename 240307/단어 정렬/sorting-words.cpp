40cc120d1a3ec5f6cb51a6dc3ebe5ca18f672b5a
SangwooKim-0512 <161012224+SangwooKim-0512@users.noreply.github.com>
2024-03-07 17:50:06 +0900

    Title: 단어 정렬, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240307/단어 정렬/sorting-words.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string str[n];
+    for(int i = 0; i < n; i++) cin >> str[i];
+    sort(str, str+n);
+    for(int i = 0; i < n; i++) cout << str[i] << "\n";
+    return 0;
+}
