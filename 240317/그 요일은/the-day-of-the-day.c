cb6a990103e0211bf5b57c8b6696604f8f5986c0
SangwooKim-0512 <161012224+SangwooKim-0512@users.noreply.github.com>
2024-03-17 20:03:38 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240317/그 요일은/the-day-of-the-day.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+char dotw[7][4] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+int Numdays(int m, int d){
+    int total_days = 0;
+    for(int i = 1; i < m; i++){
+        total_days += days[i];
+    }
+    total_days += d;
+    return total_days;
+}
+
+int main() {
+    int m1, d1, m2, d2;
+    scanf("%d%d%d%d", &m1, &d1, &m2, &d2);
+    int ndays = Numdays(m2, d2) - Numdays(m1, d1);
+    char day_of_week[4];
+    scanf("%s", day_of_week);
+
+    int idx = 0;
+    for(int i = 0; i < 7; i++){
+        //printf("%s %s %d\n", dotw[i], day_of_week, idx);
+        if(strcmp(dotw[i], day_of_week) == 0)
+            idx = i;
+    }
+    printf("%d %d\n", ndays, idx);
+    printf("%d", ((ndays - idx) / 7) + 1);
+
+    return 0;
+}
