f559ea4255a2352e5269a3eae328af02b6859f0e
SangwooKim-0512 <161012224+SangwooKim-0512@users.noreply.github.com>
2024-03-18 21:17:51 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 17ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240318/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+//가장 작은 x1y1 인덱스와 가장 큰 x2y2인덱스를 구한다.
+
+int main() {
+    int arr[2001][2001] = {0};
+    for(int i = 0; i < 2; i++){
+        int x1, x2, y1, y2;
+        scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+        x1 += 1000; y1 += 1000; x2 += 1000; y2 += 1000;
+        for(int x = x1; x < x2; x++){
+            for(int y = y1; y < y2; y++){
+                if(i == 0){
+                    arr[x][y] ++;
+                }
+                else if(i == 1){
+                    arr[x][y] --;
+                   // printf("x : %d / y : %d / arr[x][y] : %d\n", x, y, arr[x][y]);
+                }
+            }
+        }
+    }
+    int max_x = 0, max_y = 0, min_x = 2001, min_y = 2001;
+
+
+    for(int x = 0; x < 2001; x ++){
+        for(int y = 0; y < 2001; y++){
+            if(arr[x][y] == 1){
+
+                if(x > max_x) max_x = x;
+                if(y > max_y) max_y = y;
+                if(x < min_x) min_x = x;
+                if(y < min_y) min_y = y;
+            }
+        }
+    }
+    int area = (max_x - min_x + 1) * (max_y - min_y + 1);
+    printf("%d", area);
+    return 0;
+}
