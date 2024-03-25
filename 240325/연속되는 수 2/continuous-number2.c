0d28b24e0b5589d94b845ba22dffd16de54eb8a4
SangwooKim-0512 <161012224+SangwooKim-0512@users.noreply.github.com>
2024-03-25 19:09:49 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240325/연속되는 수 2/continuous-number2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, num[1001];
+    scanf("%d%d", &n, &num[0]);
+    int cnt = 1, max = 0;
+    int arr[1001];
+
+    for(int i = 1; i < n; i++){
+        scanf("%d", &num[i]);
+        if(num[i] == num[i-1]) cnt ++;
+        else {
+            if(max < cnt) max = cnt;
+            cnt = 1;
+        }
+    }
+    printf("%d", max);
+    return 0;
+}
